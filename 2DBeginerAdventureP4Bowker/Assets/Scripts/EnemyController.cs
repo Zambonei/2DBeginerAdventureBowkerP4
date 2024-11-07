755d9a0debfed23897cc0ced025c43e300f8bdf5
Zambonei <726926@student.dusd.net>
2024-11-07 11:39:50 -0800

    animation
    i did some animating and some scripting to the enemys and player

--- 2DBeginerAdventureP4Bowker/Assets/Scripts/EnemyController.cs
+++ 2DBeginerAdventureP4Bowker/Assets/Scripts/EnemyController.cs
@@ -13,11 +13,14 @@ public class EnemyController : MonoBehaviour
 
     float timer;
     int direction = 1;
+
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -35,10 +38,14 @@ public class EnemyController : MonoBehaviour
         Vector2 position = rigidbody2d.position;
         if(vertical)
         {
+            animator.SetFloat("MoveX", 0);
+            animator.SetFloat("MoveY", direction);
             position.y = position.y + Time.deltaTime * speed * direction;
         }
         else
         {
+            animator.SetFloat("MoveX", direction);
+            animator.SetFloat("MoveY", 0);
             position.x = position.x + Time.deltaTime * speed * direction;
         }
         
