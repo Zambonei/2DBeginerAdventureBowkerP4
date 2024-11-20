4158b8b1d3755e369af0f51e482afcf2b57e6721
Zambonei <726926@student.dusd.net>
2024-11-20 11:43:05 -0800

    did some scripting and etc
    did some scripting animating and added an NPC and stuff with that

--- 2DBeginerAdventureP4Bowker/Assets/Scripts/PlayerController.cs
+++ 2DBeginerAdventureP4Bowker/Assets/Scripts/PlayerController.cs
@@ -64,6 +64,15 @@ public class PlayerController : MonoBehaviour
             Launch();
         }
 
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit2D = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if(hit2D.collider != null)
+            {
+                Debug.Log("Raycast has hit the object " + hit2D.collider.gameObject);
+            }
+        }
+
     }
     void FixedUpdate()
     {
@@ -87,7 +96,7 @@ public class PlayerController : MonoBehaviour
             invincibleTimer = timeInvincible;
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        UIHealthBar.instance.SetValue(currentHealth/(float)maxHealth);
     }
        void Launch()
     {
